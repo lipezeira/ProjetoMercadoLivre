4935097d35eb36aa9e27e090043e79bd925608e5
Felipe Ribeiro De Souza <114113854+lipezeira@users.noreply.github.com>
2023-05-22 20:27:39 -0300

    Add files via upload
    Arquivos Enviados e Feitos por felipe

---
+++ dados.php
@@ -0,0 +1,35 @@
+<?php
+
+$produtos = [   
+    [        
+        "nome" => "Bicicleta Aço Carbono Ksvj Aro 29 Freios A Disco 21 Vel",
+        "foto" => "imagens/bicicleta.jpg",
+        "preco" => 930,
+        "desconto" => "25% OFF" ,
+        "parcelas" => "10x R$119,9 sem juros",
+
+    ],
+    [       
+        "nome" => "Forno de bancada elétrico Philco PFE42P 38L preto 110V",
+        "foto" => "imagens/microondas.jpg",
+        "preco" => 371,
+        "desconto" => "25% OFF",
+        "parcelas" =>  "10x R$37,1 sem juros",
+       
+    ],
+    [   "nome" => "Smart TV Samsung UN32T4300AGXZD LED HD 32",
+        "foto" => "imagens/smarttv.jpg",  
+        "preco" => 1199,
+        "desconto" => "25% OFF" ,
+        "parcelas" =>  "10x R$119.9 sem juros",
+    ],
+    [  
+        "nome" => "Máquina de lavar automática Consul CWH12 branca 12kg 220 V", 
+        "foto" => "imagens/maquinadelavar.jpeg",
+        "preco" =>1799,
+        "desconto" => "25% OFF",
+        "parcelas" =>  "10x R$179,9 sem juros",
+    ]
+];
+
+?>
